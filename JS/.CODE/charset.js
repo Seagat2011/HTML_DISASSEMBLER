9f3dc8077543f27a3de8c5c3eb40173a7870c884
Michelle Antonello <Seagat2011@users.noreply.github.com>
2022-01-10 12:39:24 -0500

    - elide

--- JS/.CODE/charset.js
+++
@@ -1,7 +0,0 @@
-var charSet = {}
-
-"1234567890`~!@#$%^&*()-_=+\\;:'\"|,<.>/?{}[]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\t\n\a\r "
-.split("")
-.map(function(w){
-  charSet[w.charCodeAt()] = w
-})
