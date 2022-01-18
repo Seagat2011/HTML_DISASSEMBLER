abf0e3984168607e994ce25d03891f07f7cc3f2f
Michelle Antonello <Seagat2011@users.noreply.github.com>
2022-01-18 03:31:56 -0500

    Update httpServ.bat

--- JS/.CODE/httpServ.bat
+++ JS/.CODE/httpServ.bat
@@ -1,5 +1,4 @@
-echo "Starting Working directory for Chrome (CORS) compliance"
+echo "Starting Working directory http.server for Chrome (CORS) compliance"
 cd "../../"
-echo "Starting http.server"
 python -m http.server 8356 --bind 127.0.10.1
 echo "Done!"
